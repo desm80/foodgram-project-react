eaca732e0d150d32ffa861a6cea59429aa9db47b
Denis Smirnov <desm80@yandex.ru>
2022-11-29 17:36:58 +0300

    users/

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,17 +153,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.AllowAny',
-    # ),
-    # 'DEFAULT_PAGINATION_CLASS': (
-    #     'rest_framework.pagination.LimitOffsetPagination',
-    # ),
-    # 'PAGE_SIZE': 6,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 6,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_FILTER_BACKENDS':(
+    'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
 }
