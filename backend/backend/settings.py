62537e31f8cd8c8c0bfce39e9375887a19d5d9e1
Denis Smirnov <desm80@yandex.ru>
2022-11-30 21:18:22 +0300

    даунгрейдил дрф

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,32 +82,33 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME', default='postgres'),
+#         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.sqlite3'),
+#         'NAME': os.getenv(
+#             'DB_NAME', default=os.path.join(BASE_DIR, 'db.sqlite3')),
 #         'USER': os.getenv('POSTGRES_USER', default='postgres'),
 #         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
 #         'HOST': os.getenv('DB_HOST', default='localhost'),
 #         'PORT': os.getenv('DB_PORT', default=5432)
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'gfhjkm',
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'gfhjkm',
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
