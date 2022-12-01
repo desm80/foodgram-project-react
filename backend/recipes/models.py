e92de696f3311a1e130d99e470127f817fb47fa1
desm <desm80@yandex.ru>
2022-12-01 15:14:12 +0300

    начало работы с моделями recipe

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,78 @@
+from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import UniqueConstraint
+from django.utils.text import slugify
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    color = models.CharField(
+        max_length=7,
+        unique=True,
+        null=True,
+        validators=[
+            RegexValidator(
+                regex='^#(?:[0-9a-fA-F]{1,2}){3}$',
+                message='HEX color required')
+        ],
+    )
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['name', 'color'],
+                name='unique_tag')
+        ]
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Tag, self).save(*args, **kwargs)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    measurement_unit = models.CharField(max_length=200)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['name', 'measurement_unit'],
+                name='unique_ingredient')
+        ]
+
+
+class Recipe(models.Model):
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+    )
+    name = models.CharField(max_length=200)
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        null=False,
+        )
+    text = models.TextField()
+    cooking_time = models.SmallIntegerField(validators=[
+        MinValueValidator(1),
+    ])
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.SmallIntegerField(validators=[
+        MinValueValidator(1),
+    ])
