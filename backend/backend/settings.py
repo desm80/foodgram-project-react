b63b899d8afc8d755fda16d9d88805c26bcfea87
desm <desm80@yandex.ru>
2022-12-16 15:03:03 +0300

    статика

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,10 +101,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static/')
 
 MEDIA_URL = '/backend_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media/')
 
 AUTH_USER_MODEL = 'users.User'
 
